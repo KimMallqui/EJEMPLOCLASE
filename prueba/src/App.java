f19454a9306ce34819ef53079c28ae418532c410
kim <daw202416@estudiantes.salesianasnsp.es>
2023-03-13 13:23:13 +0100

    añadido nombre

--- prueba/src/App.java
+++ prueba/src/App.java
@@ -1,5 +1,12 @@
+import java.util.Scanner;
+
 public class App {
+  
     public static void main(String[] args) throws Exception {
+        Scanner sc=new Scanner(System.in);
+        
         System.out.println("Esto es una prueba GIT");
+        System.out.println("Nombre :");
+        String nombre=sc.nextLine();
     }
 }
